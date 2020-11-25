a5eaa47069bf279b0162f4b3dd8b217fcd8c2e64
daliwang <pkunewking@126.com>
2020-11-25 20:13:06 +0800

    basic layout

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from "element-ui/lib/locale";
+import VueDataTables from 'vue-data-tables'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
+locale.use(lang)
+Vue.use(VueDataTables)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-Vue.use(ElementUI)
+
