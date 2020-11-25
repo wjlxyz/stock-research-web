03e5597b49f11f5cbc8f11fc4243576168581585
daliwang <pkunewking@126.com>
2020-11-25 14:35:43 +0800

    init project

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <div class="hello">
+            <h1>{{ msg }}</h1>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+Vue.use(ElementUI)
