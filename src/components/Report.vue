bf0de369faa2675f64606569de28a93cbba7ef58
daliwang <pkunewking@126.com>
2020-11-27 20:09:19 +0800

    add report component

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,96 +1,139 @@
 <template>
     <div>
-        <div style="margin-bottom: 10px">
-            <el-row>
-                <el-col :span="18">
-                    <el-button @click="onCreate">create 1 row</el-button>
-                    <el-button @click="onCreate100">create 100 row</el-button>
-                    <el-button @click="bulkDelete">bulk delete</el-button>
-                </el-col>
-
-                <el-col :span="6">
-                    <el-input placeholder="search NO." v-model="filters[0].value"></el-input>
-                </el-col>
-            </el-row>
+        <div style="border: 1px solid #eee; padding: 10px">
+            <div>
+                <el-select v-model="brokerName" placeholder="请选择券商">
+                    <el-option
+                            v-for="item in brokers"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                    </el-option>
+                </el-select>
+            </div>
+            <div>
+                <p>{{response}}</p>
+                <span class="demonstration">请选择时间范围</span>
+                <el-date-picker
+                        v-model="dateRangeValue"
+                        type="daterange"
+                        align="right"
+                        unlink-panels
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        format="yyyy 年 MM 月 dd 日"
+                        value-format="yyyy-MM-dd"
+                        :picker-options="pickerOptions">
+                </el-date-picker>
+            </div>
+            <div style="margin-bottom: 10px">
+                <el-row>
+                    <el-col :span="6">
+                        <el-input v-model="filters[0].value" placeholder="输入研报标题关键字"></el-input>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="6">
+                        <el-input v-model="filters[1].value" placeholder="输入买入评级关键字"></el-input>
+                    </el-col>
+                </el-row>
+            </div>
         </div>
 
-        <data-tables :data="data" :action-col="actionCol" :filters="filters" @selection-change="handleSelectionChange">
-            <el-table-column type="selection" width="55"></el-table-column>
-
-            <el-table-column v-for="title in titles" :prop="title.prop" :label="title.label" :key="title.prop" sortable="custom">
-            </el-table-column>
-        </data-tables>
+        <div>
+            <data-tables :data="data" :filters="filters">
+                <el-table-column
+                        v-for="title in titles"
+                        :prop="title.prop"
+                        :label="title.label"
+                        :key="title.prop">
+                </el-table-column>
+            </data-tables>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from "axios";
+
     export default {
         name: "Report",
+        created() {
+            const url = 'http://reportapi.eastmoney.com/report/jg?'
+                + '&pageSize=5'
+                + '&beginTime='
+                + '&endTime='
+                + '&pageNo=1'
+                + '&fields='
+                + '&qType=3'
+                + '&orgCode=80055521'
+                + '&_=1605007898254'
+            axios.get(url).then(response => {
+                this.response = response;
+                console.log(response)
+            })
+
+        },
         data() {
             return {
+                brokers: [
+                    {value: '0', label: 'all'}
+                ],
+                brokerName: '',
+                pickerOptions: {
+                    shortcuts: [
+                        {
+                            text: '最近一周',
+                            onClick(picker) {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 1000 * 3600 * 24 * 7);
+                                picker.$emit('pick', [start, end]);
+                            }
+                        }, {
+                            text: '最近一个月',
+                            onClick(picker) {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 1000 * 3600 * 24 * 30);
+                                picker.$emit('pick', [start, end]);
+                            }
+                        }, {
+                            text: '最近三个月',
+                            onClick(picker) {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 1000 * 3600 * 24 * 90);
+                                picker.$emit('pick', [start, end]);
+                            }
+                        }
+                    ]
+                },
+                dateRangeValue: '',
                 data: [
-                    {flow_no: 'FW20201125191200', content: 'First Line', flow_type: 'Help'}
+                    {report_title: '大国重器', broker_name: '平安证券', publish_date: '2020-10-11', rate: '持有'},
+                    {report_title: '大国软蛋', broker_name: '哈喽证券', publish_date: '2020-11-11', rate: '买入'}
                 ],
                 titles: [
-                    {label: 'NO.', prop: 'flow_no'},
-                    {label: 'Content', prop: 'content'},
-                    {label: 'Type', prop: 'flow_type'}
+                    {prop: 'report_title', label: '研报标题'},
+                    {prop: 'broker_name', label: '券商'},
+                    {prop: 'publish_date', label: '发布时间'},
+                    {prop: 'rate', label: '买卖评级'},
                 ],
-                filters: [{
-                    prop: 'flow_no',
-                    value: ''
-                }],
-                actionCol: {
-                    props: {
-                        label: 'Actionssss',
-                    },
-                    buttons: [{
-                        props: {
-                            type: 'primary'
-                        },
-                        handler: row => {
-                            this.$message('Edit clicked')
-                            row.flow_no = 'hello word' + Math.random()
-                            row.content = Math.random() > 0.5 ? 'Water flood' : 'Lock broken'
-                            row.flow_type = Math.random() > 0.5 ? 'Repair' : 'Help'
-                        },
-                        label: 'Edit'
+                filters: [
+                    {
+                        prop: 'report_title',
+                        value: ''
                     }, {
-                        handler: row => {
-                            this.data.splice(this.data.indexOf(row), 1)
-                            this.$message('delete success')
-                        },
-                        label: 'delete'
-                    }]
-                },
-                selectedRow: []
+                        prop: 'rate',
+                        value: ''
+                    }
+                ],
+                response: ''
             }
         },
-        methods: {
-            onCreate() {
-                this.data.push({
-                    content: "new created",
-                    flow_no: "FW201601010003" + Math.floor(Math.random() * 100),
-                    flow_type: "Help",
-                    flow_type_code: "help"
-                })
-            },
-            onCreate100() {
-                // eslint-disable-next-line no-unused-vars
-                [...new Array(100)].map(_ => {
-                    this.onCreate()
-                })
-            },
-            handleSelectionChange(val) {
-                this.selectedRow = val
-            },
-            bulkDelete() {
-                this.selectedRow.map(row => {
-                    this.data.splice(this.data.indexOf(row), 1)
-                })
-                this.$message('bulk delete success')
-            }
-        }
+        methods: {}
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import lang from 'element-ui/lib/locale/lang/en'
+import lang from 'element-ui/lib/locale/lang/zh-CN'
 import locale from "element-ui/lib/locale";
 import VueDataTables from 'vue-data-tables'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 locale.use(lang)
 Vue.use(VueDataTables)
+Vue.use(axios)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
